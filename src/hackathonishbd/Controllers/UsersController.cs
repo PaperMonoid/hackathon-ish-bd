216582429a63124c04af1083f20011a2a764fdd3
Daniel Santiago Aguila Torres <tritiummonoid@gmail.com>
2018-12-11 23:21:33 -0800

    arreglar merge

--- src/hackathonishbd/Controllers/UsersController.cs
+++ src/hackathonishbd/Controllers/UsersController.cs
@@ -12,30 +12,25 @@ namespace hackathonishbd.Controllers
 
         [HttpPost]
         [Route("index")]
-        public ActionResult Index(int ID_usuario,string Clave)
+        public ActionResult Index(int ID_usuario, string Clave)
         {
             ISession session = NHibernateHelper.GetCurrentSession();
-                using (ITransaction tx = session.BeginTransaction())
+            using (ITransaction tx = session.BeginTransaction())
+            {
+
+                var authUser = session.Query<T_usuarios>()
+                                       .Where(x => x.ID_usuario == ID_usuario)
+                                       .FirstOrDefault();
+                if (authUser != null)
                 {
-
-                    var authUser = session.Query<T_usuarios>()
-                                           .Where(x => x.ID_usuario == ID_usuario )
-                                           .FirstOrDefault();
-                    if (authUser != null)
-                    {
-                        FormsAuthentication.SetAuthCookie(authUser.Nombre, false);
-                NHibernateHelper.CloseSession();
-                        return RedirectToAction("", "Dashboard");
-                    }
-
-                    return RedirectToAction("", "");
+                    FormsAuthentication.SetAuthCookie(authUser.Nombre, false);
+                    NHibernateHelper.CloseSession();
+                    return RedirectToAction("", "Dashboard");
                 }
-<<<<<<< HEAD
 
+                return RedirectToAction("", "");
             }
-            return View();
-=======
->>>>>>> 69b0a263fa5fca74ca2a38920d2faeb8b4bfbec2
+
         }
     }
 }
