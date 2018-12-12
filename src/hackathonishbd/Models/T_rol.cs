9e73527b4508ee7747dcea5df01a6ffd033c2c78
pablo-aguiar <pablo.aguiar17@tectijuana.edu.mx>
2018-12-11 16:42:00 -0800

    Modelos y algo de xml.

---
+++ src/hackathonishbd/Models/T_rol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hackathonishbd.Models
+{
+    public class T_rol
+    {
+        public virtual int ID_rol { get; set; }
+        public virtual string Descripcion { get; set; }
+    }
+}
